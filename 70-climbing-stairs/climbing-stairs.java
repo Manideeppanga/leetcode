3c6ce175578fbb94892536c12c596fdc0be4a4d8
Manideeppanga <manipanga14@gmail.com>
2026-08-20 13:15:12 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (36.55%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int ways(int n,int[] dp){
+        if(n == 1 || n == 2){
+            return n;
+        }
+        if(dp[n] != 0){
+            return dp[n];
+        }
+        dp[n] = ways(n - 1,dp) + ways(n - 2,dp);
+        return dp[n];
+    }
+    public int climbStairs(int n) {
+        int[] dp = new int[n + 1];
+        return ways(n,dp);
+    }
+}
