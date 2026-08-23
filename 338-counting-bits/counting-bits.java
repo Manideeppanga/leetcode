531f454124ab7aefb2cafdd003ba923c383d1c95
Manideeppanga <manipanga14@gmail.com>
2026-08-23 21:34:37 +0530

    Time: 4 ms (23.03%) | Memory: 48.7 MB (31.67%) - LeetSync

---
+++ 338-counting-bits/counting-bits.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int[] countBits(int n) {
+        if(n == 0){
+            return new int[]{0};
+        }
+        if(n == 1){
+            return new int[]{0,1};
+        }
+        if(n == 2){
+            return new int[]{0,1,1};
+        }
+        int[] ans = new int[n + 1];
+        ans[0] = 0;
+        ans[1] = 1;
+        ans[2] = 1;
+        for(int i = 3;i < ans.length;i++){
+            int last = Integer.highestOneBit(i);
+            if(last == i){
+                ans[i] = 1;
+            }
+            ans[i] = ans[last] + ans[i - last];
+        }
+        return ans;
+    }
+}
