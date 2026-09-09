44f0e28c834f6bdd50663a7892d0294f6b5760e8
Manideeppanga <manipanga14@gmail.com>
2026-09-09 22:18:01 +0530

    Time: 1 ms (99.36%) | Memory: 42.9 MB (11.54%) - LeetSync

---
+++ 4248-count-commas-in-range-ii/count-commas-in-range-ii.java
@@ -0,0 +1,24 @@
+class Solution {
+    public long countCommas(long n) {
+        if(n <= 999){
+            return 0;
+        }
+        long ans = 0;
+        if(n > 999){
+             ans += (n - 999);
+        }
+        if(n > 999999){
+            ans += (n - 999999);
+        }
+        if(n > 999999999){
+            ans += (n - 999999999);
+        }
+        if(n > 999999999999L){
+            ans += (n - 999999999999L);
+        }
+        if(n > 999999999999999L){
+            ans += (n - 999999999999999L);
+        }
+        return ans;
+    }
+}
